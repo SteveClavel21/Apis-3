e275220af961322a08cd5b06b9f143b46f37adb3
German Steven Méndez Clavel <clavelsteven441@gmail.com>
2024-09-03 20:38:00 -0600

    Agregar archivos de proyecto.

---
+++ GSMC20240903/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GSMC20240903.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        // GET: api/<AccountController>
+        [HttpPost("login")]
+        public IActionResult Login(string login, string password)
+        {
+            if (login == "admin" && password == "123")
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, login),
+                };
+
+                var claimsIdentity = new ClaimsIdentity(claims,
+                    CookieAuthenticationDefaults.AuthenticationScheme);
+
+                var authProperties = new AuthenticationProperties
+                {
+
+                };
+
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity), authProperties);
+
+                return Ok("Inicio sesion");
+            }
+            else return Unauthorized("Incorrecto");
+        }
+
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Ok("Cerrado");
+        }
+
+    }
+}
---
+++ GSMC20240903/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.ReturnUrlParameter = "unauthorized";
+        options.Events = new CookieAuthenticationEvents
+        {
+            OnRedirectToLogin = context =>
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+
+                context.Response.ContentType = "application/json";
+
+                var messege = new
+                {
+                    error = "No autorizado",
+                    messege = "Debe de iniciar sesion"
+                };
+                var jsonMessege = JsonSerializer.Serialize(messege);
+                return context.Response.WriteAsync(jsonMessege);
+            }
+        };
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
